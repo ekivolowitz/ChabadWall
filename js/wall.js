508ef4235eea5ed3b97d6ef74154d197dfe549ad
Evan Kivolowitz <ekivolowitz@wisc.edu>
2017-05-21 22:23:20 -0500

    Registered clicking

--- js/wall.js
+++ js/wall.js
@@ -8,7 +8,8 @@ function main()
     base_image.onload = function(){
         c.drawImage(base_image, 0,0);
     }
-    c.drawImage("photos/wall.jpeg", 0,0);
+    // c.drawImage("photos/wall.jpeg", 0,0);
+    loop();
 }
 
 
@@ -16,4 +17,20 @@ function draw()
 {
     var userInput = document.getElementById('name').value;
     
+}
+
+function loop(){
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext("2d");
+    canvas.addEventListener('click', function(e)
+    {
+        var radius = 70;
+        context.beginPath();
+        context.arc(e.offsetX, e.offsetY, 20, 0, 2 * Math.PI, false);
+        context.fillStyle = 'green';
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = '#003300';
+        context.stroke();
+    });
 }
