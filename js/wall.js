7b445b8573fa901232fcb1cadf4363f044e55571
Evan Kivolowitz <ekivolowitz@wisc.edu>
2017-06-19 22:06:46 -0500

    Drawing on click works. Now working on a border around the cursor to show where the text would appear.

--- js/wall.js
+++ js/wall.js
@@ -1,36 +1,35 @@
 
-function main()
-{
+function main(){
     var canvas = document.getElementById('myCanvas');
+    // var canvas = this.canvas;
     var c = canvas.getContext('2d');
     var base_image = new Image();
     base_image.src = 'photos/wall.jpeg';
     base_image.onload = function(){
         c.drawImage(base_image, 0,0);
     }
-    // c.drawImage("photos/wall.jpeg", 0,0);
-    loop();
+    canvas.addEventListener('mousemove', function(e) { drawBorder(e);}, false);
+    canvas.addEventListener("click", function(e) { loop(e);}, false);
+   
 }
 
-
-function draw()
-{
-    var userInput = document.getElementById('name').value;
-    
-}
-
-function loop(){
+function drawBorder(e){
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext("2d");
-    canvas.addEventListener('click', function(e)
-    {
-        var radius = 70;
-        context.beginPath();
-        context.arc(e.offsetX, e.offsetY, 20, 0, 2 * Math.PI, false);
-        context.fillStyle = 'green';
-        context.fill();
-        context.lineWidth = 5;
-        context.strokeStyle = '#003300';
-        context.stroke();
-    });
+    var currX = e.offsetX;
+    var currY = e.offsetY;
+    context.rect(e.offsetX, e.offsetY, 20, 20);
+    context.stroke();
+    context.clearRect(e.offsetX, e.offsetY, 20, 20);
+
 }
+
+
+function loop(e){
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext("2d");
+    var text = document.getElementById('name').value;
+    context.font =  "30px Arial";
+    context.fillText(text, e.offsetX, e.offsetY);
+}
+
