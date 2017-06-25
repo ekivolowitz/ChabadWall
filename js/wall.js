2904e6c2dfdb1402ff9b72bdca535f39310828c6
Evan Kivolowitz <ekivolowitz@wisc.edu>
2017-06-24 21:57:24 -0500

    One name at a time.

--- js/wall.js
+++ js/wall.js
@@ -1,33 +1,56 @@
 
 function main(){
     var canvas = document.getElementById('myCanvas');
-    // var canvas = this.canvas;
     var c = canvas.getContext('2d'); 
+    
+    var latest; // defined in drawName
+    console.log(latest);
     var square = {
     	x : canvas.width / 2,
     	y : canvas.height / 2,
-    	width : 20,
+    	width : 218,
     	height : 20
     };
+
     canvas.addEventListener('mousemove', function(e) {move(e, square)}, false);
-}
+    canvas.addEventListener("click", function(e) { drawName(e);}, false);
 
-function redraw(square){
-	var canvas = document.getElementById('myCanvas');
-    var c = canvas.getContext('2d');
-	c.beginPath();
-	c.clearRect(0, 0, canvas.width, canvas.height);
-	c.strokeStyle = "rgba(0,255,0,255)";
-	c.stroke();
-	c.rect(square.x, square.y, square.width, square.height);
-	c.stroke();
-	c.closePath();
-}
+    // Begin helper functions. 
 
-function move(e, square){
-	square.x = e.clientX - square.width;
-	square.y = e.clientY - square.height;
-	redraw(square);
-}
+    /**
+     *	Function renders the square around the cursor, as well as the
+     *	latest text entry.
+     *	@param square the latest square to draw.
+     */
+	var redraw = function(square){
+		c.clearRect(0, 0, canvas.width, canvas.height);
+		if(latest != undefined){
+			c.fillText(latest.t, latest.x, latest.y);
+		}
+		c.beginPath();
+		c.strokeStyle = "rgba(0,255,0,255)";
+		c.stroke();
+		c.rect(square.x, square.y, square.width, square.height);
+		c.stroke();
+		c.closePath();
+	}
 
+	var move = function(e, square){
+		square.x = e.clientX - 8;
+		square.y = e.clientY - 15;
+		redraw(square);
+	}
+
+	var drawName = function(e){
+		c.clearRect(0, 0, canvas.width, canvas.height);
+		var text = document.getElementById('name').value;
+	 	c.font =  "30px Arial";
+	 	latest = {
+	 		x : e.offsetX,
+	 		y : e.offsetY,
+	 		t : text
+	 	}
+	 	c.fillText(latest.t, latest.x, latest.y);
+	}
+}
 
